f08d0599f75632400f12ad7ad181e815d7c59e0e
Tatyana Danilchik <tanike18@Gmail.com>
2024-01-23 09:24:46 +0600

    add tests with selenium

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -8,7 +10,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void login() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -19,7 +21,35 @@ public class LoginTests extends BaseTest {
 
         String url = "https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        emailInput.sendKeys("demo@class.com");
+        passwordInput.sendKeys("te$t$tudent");
+        loginButton.click();
+        WebElement avatar = driver.findElement(By.cssSelector("#userBadge img"));
+        Assert.assertTrue(avatar.isDisplayed()); // true
+        driver.quit();
+    }
+
+    @Test
+    public void loginWithEmptyCredentials() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement logo = driver.findElement(By.cssSelector(".logo"));
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement loginButton = driver.findElement(By.cssSelector("[type='submit']"));
+        emailInput.sendKeys("demo@class.com");
+        loginButton.click();
+
+        Assert.assertTrue(logo.isDisplayed());
         driver.quit();
     }
 }
