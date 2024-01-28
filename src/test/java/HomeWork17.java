d2c57bb6e2c525fe1cf9a18817be87f84c9f569b
fAnt3eRr <fanta3er.yu@gmail.com>
2024-01-28 13:57:49 -0600

    HW17YU

--- src/test/java/HomeWork17.java
+++ src/test/java/HomeWork17.java
@@ -7,28 +7,25 @@ public class HomeWork17 extends BaseTest {
 
 
     @Test
-    public void addSongToPlaylist() {
+    public void addSongToPlaylist() throws InterruptedException {
 
         login("Yevhenii.Ustenko@testpro.io", "Fantazer120393!");
         String songName = "BornKing";
         WebElement search = driver.findElement(By.cssSelector("[type='search']"));
-        //WebElement songName = driver.findElement(By.xpath("//button[contains(@title,'BornKing')]");
         search.sendKeys("BornKing");
         WebElement viewButton = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
         viewButton.click();
         WebElement selectSong = driver.findElement(By.xpath("//td[contains(text(),'BornKing')]"));
         selectSong.click();
-        //search.sendKeys("BornKing");
         WebElement addInput = driver.findElement(By.cssSelector("[class='btn-add-to']"));
         addInput.click();
-        WebElement playlist = driver.findElement(By.xpath("//li[contains(text(),'Popmusic')][last()!=6]"));
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='playlists']//a[text()='Popmusic']"));
         playlist.click();
         WebElement popMusic = driver.findElement(By.xpath("//a[@class='active']"));
         popMusic.click();
-        //WebElement addedSong = driver.findElement(By.xpath("//td[contains(text(),'BornKing')]"));
+        Thread.sleep(3000);
         search.sendKeys("BornKing");
         Assert.assertEquals(true, true);
-        closeBrowser();
 
     }
 }
